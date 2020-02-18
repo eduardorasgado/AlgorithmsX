5b9c141769460b0582a4c7291d8886cf029d3905
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-17 23:59:48 -0600

    reviewing the first 4 steps to solve problems

--- problem-solving/introduction-to-problem-solving.js
+++ problem-solving/introduction-to-problem-solving.js
@@ -44,19 +44,92 @@
  * Labeling: string: paraph
  *           counter: dictionary {lettter: times}
  * */
+
+
+/*
+* PROBLEM SOLVING STRATEGIES
+*
+*   3. Break it Down
+*
+*       Explicitly write out the steps you need to take.
+*           This forces you to think about the code you will write before you write it, and helps you catch any
+*           lingering conceptual issues or misunderstandings before you dive in and have to worry about details
+*           (e.g. language syntax) as well
+*
+* */
+
+function charCountExplanation(str) {
+    // do something
+    // return an object with keys that are lowercase alphanumeric characters in the string;
+}
+
+function charCountPseudoCode(str) {
+    // make object to return at end
+    // loop over string, for each character...
+    // if the char is a number/letter AND a key in object, add one to count
+    // if the char is a number/letter AND not object, add it to the object and set value to 1
+    // if character is something else (space, period, etc.) dont do anything
+    // return object at end
+}
+
+/**
+ * PROBLEM SOLVING STRATEGIES
+ *      4. Solve / Simplify
+ *          If you can...
+ *          But if you can't... Solve a simpler problem
+ *
+ *          Simplify
+ *              i. Find the core difficulty in what you're trying to do
+ *              ii. Temporarily ignore that difficulty
+ *              iii. Write a simplified solution
+ *              iv. Then incorporate that difficulty back in.
+ */
+
+function charCount(str) {
+    // make object to return at end
+    let result = {}
+    // loop over string, for each character...
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase();
+        // if the char is a number/letter AND a key in object, add one to count
+        if(char.match(/^[0-9a-zA-Z]+$/)){
+            if(result[char] > 0) {
+                result[char]++;
+                // if the char is a number/letter AND not object, add it to the object and set value to 1
+            } else result[char] = 1;
+        }
+    }
+    // if character is something else (space, period, etc.) dont do anything
+    // return object at end
+    return result;
+}
+
+console.log(charCount("Your PIN code is: 1234"))
+console.log("-------------")
+
+ /*
+ * My own implementation
+ * @param paraph: string
+ * @returns {TypeError|{}|*}
+ */
 function countLetters(paraph = "") {
     // O(n)
-    arr = Array.from(paraph.toLowerCase());
-    letter_counter = {};
-    // O(n)
-    arr.forEach((element) => {
-        // On(1)
-        !letter_counter.hasOwnProperty(element) ?
-            letter_counter[element] = 1 :
-            letter_counter[element] += 1;
-    })
-    return letter_counter;
+
+    if(typeof(paraph) == "string") {
+        arr = Array.from(paraph.toLowerCase());
+        let letter_counter = {};
+        // O(n)
+        arr.forEach((e) => {
+            // On(1)
+            e.match(/^[0-9a-zA-Z]+$/) && (!letter_counter.hasOwnProperty(e) ?
+                letter_counter[e] = 1 :
+                letter_counter[e] ++);
+        })
+        return letter_counter;
+    }
+    return new TypeError("something wrong, not a string");
 }
+
 
 console.log(countLetters("aminoacids"));
 
@@ -67,3 +140,32 @@ console.log(countLetters("Can the outputs be determined from the inputs? In othe
 console.log(countLetters("aaaab 1241412"));
 console.log(countLetters(""));
 console.log(countLetters());
+console.log(countLetters(1));
+
+
+/**
+ *    PROBLEM SOLVING STRATEGIES
+ *
+ *      5. Look back and refactor
+ *
+ *          Refactoring questions
+ * */
+
+function charCount(str) {
+    // make object to return at end
+    let result = {}
+    // loop over string, for each character...
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase();
+        // if the char is a number/letter AND a key in object, add one to count
+        if(char.match(/^[0-9a-zA-Z]+$/)){
+            if(result[char] > 0) {
+                result[char]++;
+                // if the char is a number/letter AND not object, add it to the object and set value to 1
+            } else result[char] = 1;
+        }
+    }
+    // if character is something else (space, period, etc.) dont do anything
+    // return object at end
+    return result;
+}

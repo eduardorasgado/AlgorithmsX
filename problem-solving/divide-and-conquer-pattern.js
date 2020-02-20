58bcfce16f079a1eb5792f7c122b8587bf99ceb8
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-20 13:09:13 -0600

    divide and conquer pattern: binary search

---
+++ problem-solving/divide-and-conquer-pattern.js
@@ -0,0 +1,33 @@
+/**
+ *  DIVIDE AND CONQUER PATTERN
+ *      This pattern involves dividing a data set into smaller chunks and then
+ *      repeating a process with a subset of data
+ *
+ *      This pattern can trmendously decrease time complexity
+ *
+ *
+ *
+ *      BINARY SEARCH
+ *          This algorithm is applied to sorted arrays
+ *          https://en.wikipedia.org/wiki/Binary_search_algorithm
+ * */
+
+function binarySeach(arr, target) {
+    if(arr.length < 1) return -1;
+    let left = 0;
+    let right = arr.length -1;
+    let mid = 0;
+    while(left <= right) {
+        mid = Math.floor(((right + left) / 2));
+        (target > arr[mid]) && (left = mid + 1);
+        (target < arr[mid]) && (right = mid - 1);
+        if(target == arr[mid]) return mid;
+    }
+    return -1;
+}
+
+console.log(binarySeach([1,2,3,5,7,8,10,13,15,17,19,22], 13)); // 7
+console.log(binarySeach([5,12,15,18], 15)); // 2
+console.log(binarySeach([5,12,15,18], 13)); // -1
+console.log(binarySeach([], 1)) -1
+console.log("---------------");

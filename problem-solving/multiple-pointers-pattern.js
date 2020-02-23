8cd6c6d07a622e0b7209aa8607baaf174198b2d3
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-22 22:42:37 -0600

    algorithm patterns: exam p4. isSubsequence, optimal solution.

--- problem-solving/multiple-pointers-pattern.js
+++ problem-solving/multiple-pointers-pattern.js
@@ -227,3 +227,96 @@ console.log(averagePair([1,2,3], 2.5)); // true
 console.log(averagePair([1,3,3,5,6,7,10,12,19], 8)); // true
 console.log(averagePair([-1,0,3,4,5,6], 4.1)); // false
 console.log(averagePair([], 4)); // false
+
+/**
+ *  isSubsequence
+ *
+ *  Write a function called isSubsequence which takes in two strings and checks
+ *  whether the characters in the first string form a subsequence of the characters in the
+ *  second string. In other words. the function should check whether the
+ *  characters in the first string appear somewhiere in the second string,
+ *  without their order changing
+ *
+ *  own words:
+ *  we have two strings, the first should be inside of the second as a substring,
+ *  it has to follow same order than original first string, substring in string2
+ *  does not have to be consecutive in list space.
+ *
+ *  input: string to search, string 2 where we should be looking for the substring
+ *  output: return true if substring exists in string 2
+ *
+ *  Naive solution:
+ *      create a position empty list, where index of string 1 elements inside string 2 will be indexed
+ *      iterate over string 1, i as index,
+ *          iterate over string 2, j as index
+ *              if string in i position is equal to string2 in j position then,
+ *                  add j to position list
+ *                  break o avoid add another letter replication far from first encounter
+ *      create temp variable and set to first element in position list,
+ *      iterate over position list, starting from 1 index
+ *          if actual position element is smaller than temp then return false
+ *          set temp to actual element
+ *      return true
+ * */
+
+function isSubsequenceNaive(str1, str2) {
+    if(str1.length > str2.length) return false;
+    if(str1.length < 1) return false;
+    let position = []
+    // Time complexity: O(n^2), space complexity: O(n)
+    for (let i = 0; i < str1.length; i++) {
+        for (let j = 0; j < str2.length; j++) {
+            if(str1[i] == str2[j]) {position.push(j); break;}
+        }
+    }
+    let temp = position[0]
+    // TC: O(n), SC: O(1)
+    for (let i = 0; i < position.length; i++) {
+        if(position[i] < temp) return false;
+        temp = position[i];
+    }
+    return true;
+}
+
+/**
+ * Optimal Solution
+ *  i. create a orderList empty list
+ *  ii. set two pointers, target(t), searcher(s), set them to 0 and 0
+ *  iii. iterate over the string 2,
+ *      1. if target is less to string 1 length
+ *          1.1. compare if string2 in searcher position is same to str1 in target position
+ *              -if true then
+ *                  - string2 in t position will take the value of string2 in s position, t increments
+ *                    and s will be stored in orderList
+ *  iv. compare if str1 is equal to str2 from 0 index to target position
+ *      compare if orderList is sorted from min to max, return true if so
+ *
+ *  orderList = []
+ *  t 0
+ *  s 0
+ *    say helyo wlrldo
+ *
+ *    0
+ *    hello
+ * */
+function isSubsequence(str1, str2) {
+    if(str1.length < 1) return false;
+    let target = 0;
+    let searcher = 0;
+    while(searcher < str2.length && target < str1.length) {
+        (str2[searcher] == str1[target]) && ++target;
+        ++searcher;
+    }
+    return (target == str1.length )
+
+}
+
+console.log('======================')
+console.log(isSubsequence('hello', 'say hello world')) // true
+console.log(isSubsequence('sing', 'sting')) // true
+console.log(isSubsequence('abc', 'abracadabra')) // true
+console.log(isSubsequence('abc', 'acb')) // false
+console.log(isSubsequence('other', 'motherboard')) // true
+console.log(isSubsequence('othre', 'motherboard')) // false
+console.log(isSubsequence('', 'motherboard')) // false
+console.log(isSubsequence('hello', 'say helyo wlrldo')) // false

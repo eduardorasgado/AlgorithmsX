b12890d4f95fd94e37ae32353a451e9c0742770e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-02-29 01:05:31 -0600

    recursion challenges: Nested Even Sum

--- recursion/challenges.js
+++ recursion/challenges.js
@@ -139,3 +139,45 @@ function capitalizeFirst(stringsArr) {
 
 console.log("------------")
 console.log(capitalizeFirst(['car', 'taco', 'banana']));
+
+
+/**
+ *  EXAM: NESTED EVEN SUM
+ *
+ *      Write a recursive function called nestedEvenSum. Return the sum of all
+ *      even numbers in an object which may contain nested objects.
+ * */
+var obj1 = {
+    outer: 2,
+    obj: {
+        inner: 2,
+        otherObj: {
+            superInner: 2,
+            notANumber: true,
+            alsoNotANumber: "yup"
+        }
+    }
+}
+
+var obj2 = {
+    a: 2,
+    b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+    c: {c: {c: 2}, cc: 'ball', ccc: 5},
+    d: 1,
+    e: {e: {e: 2}, ee: 'car'}
+};
+
+function nestedEvenSum(object) {
+    let sum = 0;
+    for(let key in object){
+        let content = object[key];
+        if (typeof content == 'object') sum += nestedEvenSum(content);
+        else if( typeof content == 'number') sum += (content % 2 === 0) ? content : 0;
+    }
+    return sum;
+}
+
+console.log("-----------");
+console.log(nestedEvenSum(obj1)); // 6
+console.log("---");
+console.log(nestedEvenSum(obj2)); // 10

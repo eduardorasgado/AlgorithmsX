c74ac2048889ae0109f98b4ede708e167fd03a77
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-01 01:37:13 -0600

    linear search

--- searching-algorithms/searching.js
+++ searching-algorithms/searching.js
@@ -4,6 +4,33 @@ const nameData = require('./names');
 //console.log(america.states);
 //console.log(nameData.names);
 
+let states = america.states;
+let names = nameData.names;
+
+console.log(states.indexOf('Oklahoma'));
+console.log(states.indexOf('Chihuahua'));
 
 
+/**
+ * SEARCHING ALGORITHMS
+ *
+ *      Objectives
+ *          - Describe what a searching algorithm is
+ *          - Implement linear search on arrays
+ *          - Implement binary search on sorted arrays
+ *          - Implement a naive string searching algorithm
+ *          - IMplement the KMP string searching algorithm
+ *
+ *
+ * LINEAR SEARCH ALGORITHM
+ * */
 
+function searchElement(list, target) {
+    // Time complexity O(n)
+    for(let i = 0; i < list.length; i++) if(list[i] === target) return i;
+    return -1;
+}
+
+console.log(searchElement(names, 'Tyler'));
+console.log(searchElement(states, 'Iowa'));
+console.log(names.includes('Tyler'))

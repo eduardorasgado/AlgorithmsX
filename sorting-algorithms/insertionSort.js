fa8ee14b59913b176a724fc351bcda36c5cd3f89
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-05 13:02:04 -0600

    insertion sort algorithm part 1

---
+++ sorting-algorithms/insertionSort.js
@@ -0,0 +1,34 @@
+const numbers = require('./unsortedNumbers');
+/**
+ *      INSERTION SORT
+ *
+ *          Builds up the sort by gradually creating a larger left half which
+ *          is always sorted
+ * */
+let unsortedNums = numbers.unsortedNumbers;
+
+// Time complexity: O(n^2)
+function insertionSort(list) {
+    for (let i = 1; i < list.length; i++) {
+        let pos = 0;
+        // looking for new position for list[i], in left ordered window
+        for (let j = 0; j < i; j++) {
+            if(list[i] > list[j]) pos = j+1;
+        }
+        // insertig the list[i] element in pos index within left half window
+        // recolocating every element in left window on place forward, up to the i position
+        let actualVal = 0;
+        let pastValue = list[pos];
+        list[pos] = list[i];
+        for(let j=pos + 1; j <= i; j++) {
+            actualVal = list[j];
+            list[j] = pastValue;
+            pastValue = actualVal;
+        }
+        //console.log(list)
+    }
+    return list;
+}
+
+console.log(insertionSort(unsortedNums));
+console.log(insertionSort([5, 3, 7, 2, 9, 1, 6, 4, 10, 8]));

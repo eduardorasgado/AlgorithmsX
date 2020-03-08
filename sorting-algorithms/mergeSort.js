92b0ee35607c68fc2b9bf59dc68fbd1ecf780b16
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-08 02:22:48 -0600

    merge sort: undestanding merge function part 2.

--- sorting-algorithms/mergeSort.js
+++ sorting-algorithms/mergeSort.js
@@ -7,8 +7,31 @@
  *
  *              - Works by decomposing an array into smaller arrays of 0 or 1 elements,
  *                  then building up a newly sorted array
+ *
+ *      Merging arrays (merge function)
+ *          - In order to implement merge sort, it is useful to first implement a function
+ *          responsible for merging two sorted arrays
+ *          - Give two arrays which are sorted, this helper function should create a new
+ *          array which is a so sorted, and consists of all of the elements in the two
+ *          input arrays.
+ *          - This function should run in O(n + m) time and O(n + m) space and should
+ *          not modify the parameters passed to it.
+ *
+ *          Pseudocode
+ *              - Create an empty array, take a look at the smallest values in each
+ *              input array
+ *              - While there are still values we havent looked at...
+ *                  i. If the value in the first array is smaller than the value
+ *                  in the second array, push the value in the first array into our
+ *                  results and move on to the next value in the first array
+ *                  ii. If the value in the first array is larger than the value in
+ *                  the second array, push the value in the second array into our results
+ *                  and move on to the next value in the second array
+ *                  iii. Once we exhaust one array, push in all remaining values from
+ *                  the other array
  * */
 
+// time and space: O(n + m)
 function merge(left, right) {
     let newList = [];
     let i = 0;
@@ -22,8 +45,8 @@ function merge(left, right) {
     }
     // adding left elements from both lists to new list
     // those elements were'nt added. note that i and j do not restart to 0
-    // one of the indexes results in same to length it means every element in that list
-    // were inserted in newList, so while in this list will not be performed.
+    // one of the indexes results be same to length it means every element in that list
+    // were inserted in newList, so while loop in this list will not be performed.
     // example: from example above we finished with: newList[ 1, 2, 3, 5, 7, 9 ],
     // right list in index 2 which is 9, was left and it is included now.
     while(i < left.length) newList.push(left[i]), ++i;
@@ -54,4 +77,4 @@ console.log('data ready, go...')
 console.log(mergeSort(data));
 console.log(mergeSort([5, 3, 7, 2, 9, 1, 6, 4, 9, 10, 8]));
 //console.log(mergeSort([5, 3, 4, 1, 2]));
-
+console.log(merge([1, 10, 50], [2, 14, 99, 100]));

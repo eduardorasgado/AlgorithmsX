e41312312e220fe5315c2273cf78ced1588c60ed
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-14 00:37:25 -0600

    getting the idea of radix sort: own implementation, not fully optimized.

--- sorting-algorithms/radixSort.js
+++ sorting-algorithms/radixSort.js
@@ -1,3 +1,4 @@
+const numbers = require('./unsortedNumbers');
 /**
  *      RADIX SORT
  *
@@ -13,36 +14,44 @@
  *
  *          More digits means a bigger number.
  *
- *          My own first implementation of radix sort
+ *      My own first implementation of radix sort comes first:
  * */
 
 function bucket(list, digitPlace) {
     let i = 0;
-    let actualStringNum = '';
+    let actualStringLen = 0;
     // to accumulate all the elements
     let bucket = [...Array(10)].map(e => Array(0));
     while (i< list.length){
-        // https://stackoverflow.com/questions/13955738/javascript-get-the-second-digit-from-a-number
-        let actualStringLen = Math.max(Math.floor(Math.log10(Math.abs(list[i]))), 0) + 1
+        https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript/28203456#28203456
+        actualStringLen = Math.max(Math.floor(Math.log10(Math.abs(list))), 0) + 1;
         if(actualStringLen - 1 < digitPlace){
             // locate all numbers with a length bellow the digit place in the beggining of the list
             bucket[0].push(list[i]);
         }
         else {
+            // https://stackoverflow.com/questions/13955738/javascript-get-the-second-digit-from-a-number
             let actualElementNth = Math.floor((list[i] / Math.pow(10, digitPlace)) % 10);
-            bucket[actualElementNth].push(list[i])
+            bucket[actualElementNth].push(list[i]);
         }
         ++i;
     }
-    //bucket.flat(1);
-    console.log(bucket);
+
+    return bucket.flat(1);
+
 }
 
-function radixSortCustom(list, digitPlace = 0) {
+function radixSortCustom(list, actualStringLen = 0, digitPlace = 0) {
     // digit place go from 0 up to the list max element length
-    bucket(list, digitPlace);
+    if(digitPlace == 0) actualStringLen = 6;
+    console.log(digitPlace)
+    if(digitPlace + 1 == actualStringLen) return list;
+    return radixSortCustom(bucket(list, digitPlace), actualStringLen, ++digitPlace);
 }
 
+let unsortedNums = numbers.unsortedNumbers;
+unsortedNums.splice(0, Math.floor(unsortedNums.length / 2))
 console.log(radixSortCustom([170, 45, 75, 90, 802, 24, 2, 66]));
+console.log(radixSortCustom(unsortedNums));
 
 

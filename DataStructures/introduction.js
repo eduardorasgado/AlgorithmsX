d3e115d9de9737a2c1e96da7b51dd5f56eb41492
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-15 23:44:41 -0600

    data structures: classes, instance and class methods.

--- DataStructures/introduction.js
+++ DataStructures/introduction.js
@@ -19,45 +19,67 @@
  *          Data structures are collections of values, the relationships among them
  *          and the functions or operations that can be applied to the data
  *
+ *      WHY SO MANY?
+ *
+ *          Different data structures excel at different things. Some are highly
+ *          specialized, while others(like arrays) are more generally used.
+ *
+ *      WHY CARE?
+ *
+ *          The more time you spend as a developer, the more likelu you will need
+ *          to use one of these data structures.
+ *
+ *          You have already worked with many of them unknowingly.
+ *
+ *          And of course... interviews.
+ *
+ *      USES OF DATA STRUCTURES
+ *
+ *          There is no one "BEST" data structure. The all excel in different situations.
+ *          Otherwise why bother learning all of them.
+ *
+ *          Working with map/ location data? Use a graph!
+ *          Need an ordered list with fast inserts/
+ *              removals at the beginning and end? Use a linked list!
+ *          Web scraping nested html? Use a tree
+ *          Need to write a scheduler(with priorities)? Use a binary heap!
+ *
  * */
 
-class LinkedList {
-    constructor(size) {
-        this.size = size;
-        this.count = 0
-        this.bucket = null;
-        this.createList();
+
+/**
+ * CLASSES: BLUEPRINT TO USE FOR DATA STRUCTURES
+ *
+ *  A blueprint for creating objects with pre-defined properties and methods.
+ *
+ *  Classes in JS are just syntactic sugar and we have prototypes behind the scenes.
+ *
+ *  We are going to implmement data structures as classes.
+ * */
+
+class Point {
+    constructor(x, y, name = '') {
+        this.x = x;
+        this.y = y;
+        this.name = name;
     }
 
-    createList() {
-        this.bucket = new Array()
+    // class method
+    static distance(a, b){
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+        const sum = (dx * dx) + (dy * dy);
+        const hyp = Math.sqrt(sum)
+        return hyp;
     }
 
-    getSize(){
-        return this.size;
-    }
-
-    append(element){
-        if(typeof element == 'number') {
-            if(this.count+1 <= this.size){
-                this.bucket.push(element);
-                ++this.count;
-            }
-        }
-    }
-
-    show(){
-        console.log(this.bucket)
+    // instance method
+    setPointName(name) {
+        this.name = name;
     }
 }
 
-let mylist = new LinkedList(5);
-mylist.append(6);
-mylist.append(7);
-mylist.append(8);
-mylist.append(8);
-mylist.append(9);
-mylist.append(10);
-mylist.append(12);
-mylist.show();
-console.log(mylist.getSize());
+p1 = new Point(5, 5);
+p2 = new Point(10, 10);
+
+console.log(Point.distance(p1, p2));

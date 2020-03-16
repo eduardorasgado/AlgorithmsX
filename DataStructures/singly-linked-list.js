4bbfbc70ab9e8c99aecc76558e6fba2db42191f4
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-16 03:21:45 -0600

    singly linked lists: push method.

--- DataStructures/singly-linked-list.js
+++ DataStructures/singly-linked-list.js
@@ -47,12 +47,33 @@ class Node {
 class SinglyLinkedList {
 
     constructor() {
+        // pointers
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
-    push(number) {
-        let node = new Node(number);
+    /**
+     * Adding a new node at the end of the linked list
+     *
+     * This function should accept a value
+     * Create a new node using the value passed to the function
+     * If there is no head property on the list, set the head and tail to be the
+     * newly created node
+     * Otherwise set the next property on the tail to be the new
+     * */
+    push(value) {
+        let node = new Node(value);
+        if(this.head == null) {
+            this.head = node;
+            this.tail = node;
+        }
+        else {
+            // we just traverse the last node to avoid traversing the whole list
+            this.tail.setNext(node)
+            this.tail = node
+        }
+        ++this.length;
     }
 
     // settters and getters
@@ -60,9 +81,22 @@ class SinglyLinkedList {
     setHead(newHead){this.head = newHead}
     getTail(){return this.tail}
     setTail(newTail){this.tail = newTail}
+
+    toString(){
+        console.log('node is: ', this.getHead());
+    }
 }
 
 let first = new Node(1);
 first.setNext(new Node(2))
 first.getNext().setNext(new Node(3))
 first.toString()
+console.log("--------");
+
+let l1 = new SinglyLinkedList();
+l1.push(1)
+l1.push(10)
+l1.push(100)
+l1.push(1000)
+l1.push(2000)
+l1.toString()

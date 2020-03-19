df20c3faf973bf92ca777f2000ea0085beed5062
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-19 17:05:27 -0600

    singly linked lists: Remove method - pseudocode

--- DataStructures/singlyLinkedList/singly-linked-list.js
+++ DataStructures/singlyLinkedList/singly-linked-list.js
@@ -249,6 +249,15 @@ class SinglyLinkedList {
     /**
      * Removing a node from the linked list at a specific position
      *
+     *  If the index is less than zero or greater than the length, return
+     *      undefined
+     *  If the index is the same as the length - 1 pop
+     *  If the index is 0 shift
+     *  Otherwise, using th eget method, access the node at the index - 1
+     *  Set the next property on that node to be the next of the next node
+     *  Decrement the length
+     *  Return the value of the node removed.
+     *
      * @param index
      * @returns {null}
      */

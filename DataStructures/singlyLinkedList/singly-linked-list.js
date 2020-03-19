47036521d97df5033a9c90ac04e43e83c60b91de
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-19 00:00:29 -0600

    singly linked lists: Insert method: pseudocode

--- DataStructures/singlyLinkedList/singly-linked-list.js
+++ DataStructures/singlyLinkedList/singly-linked-list.js
@@ -217,6 +217,15 @@ class SinglyLinkedList {
     /**
      * Adding a node to the linked list at a specific location
      *
+     *  If the index is less than zero or greater than the length, return false
+     *  If the index is the same as the length, push a new node to the end of the list
+     *  If the index is zero, unshift a new node to the start of the list
+     *  Otherwise using the GET method, access the node at the index - 1
+     *  Set the next property on that node to be the new node
+     *  Set the next property on the new node to be the previous next
+     *  Increment the length
+     *  Return true
+     *
      * @param index
      * @param value
      * @returns {boolean}

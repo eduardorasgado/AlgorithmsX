035fffdf056439320345e066dc79988acea9d668
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-19 22:42:08 -0600

    doubly linked lists: push method.

--- DataStructures/doublyLinkedList/doubly-linked-list.js
+++ DataStructures/doublyLinkedList/doubly-linked-list.js
@@ -1,4 +1,4 @@
-
+const utils = require('../logUtils')
 /**
  *      DOUBLY LINKED LISTS
  *
@@ -36,12 +36,17 @@ class Node {
         this.prev = null;
     }
 
+    // GETTERS AND SETTERS
     getValue() {return this.value;}
     setValue(value) {this.value = value;}
     getNext() {return this.next;}
     setNext(next) {this.next = next;}
     getPrev() {return this.prev;}
     setPrev(prev) {this.prev = prev;}
+
+    toString() {
+        console.log(utils.inspectObject(this))
+    }
 
 }
 
@@ -52,11 +57,35 @@ class DoublyLinkedList {
         this.length = 0;
     }
 
+    // GETTERS AND SETTERS
     getHead() {return this.head;}
     setHead(head) {this.head = head;}
     getTail() {return this.tail;}
     setTail(tail) {this.tail = tail;}
     getLength() {return this.length;}
+
+    toString() {
+        console.log(utils.inspectObject(this))
+    }
+
+    /**
+     * Adding a node to the end of the doubly linked list
+     *
+     * @param value
+     * @returns {DoublyLinkedList}
+     */
+    push(value) {
+        let node = new Node(value);
+        if(!this.length) this.head = this.tail = node;
+        else {
+            let lastNode = this.tail;
+            this.tail = node;
+            this.tail.setPrev(lastNode);
+            lastNode.setNext(this.tail);
+        }
+        ++this.length;
+        return this;
+    }
 }
 
 exports.DoublyLinkedList = DoublyLinkedList;
--- DataStructures/doublyLinkedList/testing.js
+++ DataStructures/doublyLinkedList/testing.js
@@ -1,12 +1,17 @@
 const { DoublyLinkedList, Node } = require('./doubly-linked-list');
 let numbers = require('../unsortedNumbers');
 
-let node = new Node(1);
-let node2 = new Node(2);
-node2.setPrev(node);
-node.setNext(node2);
-
-console.log(node);
+//let node = new Node(1);
+//let node2 = new Node(2);
+//node2.setPrev(node);
+//node.setNext(node2);
+//node.toString();
 
 let l1 = new DoublyLinkedList();
 
+console.log("=========PUSHING===========");
+[1, 10, 100, 1000].map((n) =>{
+    l1.push(n);
+})
+
+l1.toString();

8a130c65ffa638c0927de860b5045264680ca5ca
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-21 19:52:36 -0600

    doubly linked lists: exam.

---
+++ DataStructures/doublyLinkedList/dll.js
@@ -0,0 +1,109 @@
+const util = require('util');
+
+const inspectObject = (object) => {
+    console.log(util.inspect(object,
+        {showHidden: false, depth: null, colors: true}));
+}
+
+class Node {
+    constructor(val){
+        this.val = val
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(value){
+        let node = new Node(value)
+        if(!this.length) {this.head = node; this.tail = node;}
+        else {
+            node.prev = this.tail;
+            this.tail.next = node
+            this.tail = node;
+        }
+        ++this.length;
+        return this;
+    }
+
+    unshift(value) {
+        let node = new Node(value);
+        if(!this.head) this.head = this.tail = node;
+        else {
+            node.next = this.head;
+            this.head.prev = node;
+            this.head = node;
+        }
+        ++this.length;
+        return this;
+    }
+
+    shift(){
+        if(!this.head) return undefined;
+        let lastHead = this.head;
+        this.head = lastHead.next;
+        this.head.prev = null;
+        lastHead.next = null;
+        --this.length;
+        return lastHead;
+    }
+
+    get(index) {
+        if(!this.head) return null;
+        if(index < 0 || index >= this.length) return null;
+        let half = Math.floor(this.length / 2);
+        let i;
+        let node;
+        if(index < half) {
+            i = 0;
+            node = this.head;
+            while(i < index) {
+                node = node.next;
+                ++i;
+            }
+        } else {
+            i = this.length - 1;
+            node = this.tail;
+            while(i > index) {
+                node = node.prev;
+                --i;
+            }
+        }
+        return node;
+    }
+
+    set(index, value) {
+        let node = this.get(index)
+        if(!node) return false;
+        node.val = value;
+        return true;
+    }
+
+}
+
+let node = new Node(3);
+let l1 = new DoublyLinkedList();
+l1.push(5)
+l1.push(10)
+l1.push(14)
+l1.unshift(4)
+l1.unshift(3)
+l1.shift();
+inspectObject(l1.shift());
+l1.unshift(4)
+l1.unshift(3)
+l1.unshift(2)
+l1.unshift(1)
+l1.unshift(0)
+inspectObject(l1);
+inspectObject(l1.get(4));
+l1.set(4, 444)
+inspectObject(l1);
+
+

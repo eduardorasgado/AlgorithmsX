923132d15c7b36ddb3865b4cdff97c65cad40bff
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-23 19:13:54 -0600

    tree: trees and binary search tree introduction

---
+++ DataStructures/trees/trees.js
@@ -0,0 +1,162 @@
+/**
+ *      TREES
+ *
+ *          OBJECTIVES
+ *              Define what a tree is
+ *              Compare and contrast trees and lists.
+ *              Explain the differences between trees, binary trees and
+ *                  binary search trees.
+ *              Implement operations on a binary search tree(BST)
+ *
+ *          WHAT IS A TREE
+ *
+ *              A data structure that consists of nodes in a parent/child
+ *              relationship.
+ *
+ *              A tree can look like this:
+ *
+ *                 _______2__________
+ *                 |      |         |
+ *              ___9__  __1__    __11_____
+ *              |    |  |   |   |    |   |
+ *              2    1  7  44  55   87  37
+ *
+ *          LIST AND TREE COMPARISON
+ *
+ *              List - linear
+ *              Tree - NON Linear
+ *
+ *          TREE TERMINOLOGY
+ *
+ *              Root - The top node in a tree.
+ *              Child - A node directly connected to another node when moving away
+ *                  from the root.
+ *              Parent - The converse notion of a child.
+ *              Siblings - A group of nodes with the same parent.
+ *              Leaf - A node with no children.
+ *              Edge - The connection between one node and another.
+ *
+ *          USE CASES
+ *
+ *              - HTML DOM,
+ *                  <body>
+ *                      <head>
+ *                          <meta>
+ *                          </meta>
+ *                          <link></link>
+ *                          <title></title>
+ *                      </head>
+ *                      <body>
+ *                          <div>
+ *                              ...
+ *                          </div>
+ *                      </body>
+ *                  </body>
+ *
+ *              - Network Routing
+ *                  Unicast,
+ *                  Broadcasting,
+ *                  Multicast,
+ *                  Anycast, etc
+ *
+ *              - Abstract syntax trees
+ *
+ *               It is a representation of tokens generated from statements and
+ *               expressions in a programming language. With the AST, the interpreter
+ *               or the compiler can generate machine code or evaluate an instruction.
+ *
+ *               Example:
+ *
+ *               1 + 2
+ *
+ *               This will be represented in AST like this:
+ *
+ *               + BinaryExpression
+ *                  - type: +
+ *                  - left_value:
+ *                      LiteralExpr:
+ *                      value: 1
+ *                  - right_value:
+ *                      LiteralExpr:
+ *                      value: 2
+ *
+ *              - Artificial Intelligence
+ *              - Folders in Operatng Systems
+ *              - Computer File system
+ *              - JSON
+ *
+ *          DIFFERENT TYPES OF TREES
+ *
+ *              - AA Tree
+ *              - AVL Tree
+ *              - Binary Search Tree
+ *              - Binary Tree
+ *              - Cartesian Tree
+ *              Left-child right-sibling binary tree
+ *              - Order Statistic tree
+ *              - Pagoda
+ *              - Randomized Binary Search Tree
+ *              - Red-black tree
+ *              - Rope
+ *              - Scapegoat tree
+ *              - Self balancing binary Seach tree
+ *              - Splay tree
+ *              - T- tree
+ *              - Tango Tree
+ *              - THreaded Binary Tree
+ *              - Top tree
+ *              - Treap
+ *              - WAVL tree
+ *              - Weight-balanced tree
+ *              - B Trees(B-tree, B+ Tree, B* -tree, etc)
+ *              - Heaps
+ *              - Radix, Suffic, Suffix array, Compressed suffix array,... trees
+ *              - Multiway trees(Ternary, K-ary, (a,b), Link,... trees)
+ *              - Space partition trees
+ *              - Application-specific trees
+ *
+ *          BINARY SEARCH TREES
+ *
+ *              Each node can have at most two children at the bianry part,
+ *              that means 0, 1 or 2 children for parent.
+ *
+ *              There is an ordered pattern in the whole tree
+ *
+ *              For example:
+ *
+ *                 _____10_____
+ *                 |          |
+ *              ___6__       15
+ *              |    |        |
+ *              3    8        20
+ *
+ *          HOW BINARY SEARCH TREE WORK
+ *
+ *              Every parent node has at most two children
+ *
+ *              Every node to the left of a parent node is always less than the
+ *              parent
+ *
+ *              Every node to the right of a parent node is always greater than
+ *              the parent
+ *
+ *              Next is not a binary search tree:
+ *
+ *                 _____10_____
+ *                 |          |
+ *              ___8__        25
+ *              |    |        |
+ *              3    6        20
+ *
+ *              because it is unordered.
+ *
+ *              Next is neither a BST:
+ *                 _______10_________
+ *                 |      |         |
+ *              ___8__    4        15
+ *              |    |             |
+ *              2    1            37
+ *
+ *              because it has three children  in the first row
+ *
+ * */

423f0e42bbeb3e783f6d54294ba848baa5428725
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-23 22:33:59 -0600

    binary search tree: insert method. Recursion into inserting helper method. Testing the inserting method agains duplicate elements.

--- DataStructures/trees/binary-search-tree.js
+++ DataStructures/trees/binary-search-tree.js
@@ -21,6 +21,47 @@ class BinarySearchTree {
         this.root = null;
     }
 
+    /**
+     * Main inserting function, get the conditions to call the 'inserting' method.
+     * related to the other nodes
+     * @param value
+     * @returns {boolean}
+     */
+    insert(value){
+        let newNode = new Node(value);
+        if(!this.root) this.root = newNode;
+        else {
+            let node = this.root;
+            node = this.inserting(newNode, node);
+            // cannot insert duplicates
+            if(!node) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Inserting an element within the tree, the position is actually sorted
+     * @param newNode
+     * @param node
+     * @returns {null|*}
+     */
+    inserting(newNode, node) {
+        if(node.getValue() === newNode.getValue()) return null;
+        else if(newNode.getValue() < node.getValue()) {
+            if(node.getLeft() != null)
+                return this.inserting(newNode, node.getLeft());
+            node.setLeft(newNode);
+            node = node.getLeft();
+        }
+        else if(newNode.getValue() > node.getValue()) {
+            if(node.getRight() != null)
+                return this.inserting(newNode, node.getRight());
+            node.setRight(newNode);
+            node =node.getRight();
+        }
+        return node;
+    }
+
     getroot() {return this.root;}
     setroot(root) {this.root = root;}
 
--- DataStructures/trees/bst-testing.js
+++ DataStructures/trees/bst-testing.js
@@ -2,9 +2,15 @@ const { BinarySearchTree, Node } = require("./binary-search-tree");
 console.log("======binary search tree======");
 
 let bst = new BinarySearchTree();
-bst.root = new Node(22);
-bst.root.left = new Node(6);
-bst.root.right = new Node(31);
-bst.root.left.setLeft(new Node(3));
-bst.getroot().getLeft().setRight(new Node(10));
+// bst.root = new Node(22);
+// bst.root.left = new Node(6);
+// bst.root.right = new Node(31);
+// bst.root.left.setLeft(new Node(3));
+// bst.getroot().getLeft().setRight(new Node(10));
+// bst.toString();
+let nums = [20, 22, 10, 3, 4, 15, 21, 29, 25, 33, 1, 32, 5, 6, 9, 7];
+//let nums = [10, 15, 6, 20, 3, 8, 15, 10];
+nums.map((n) => {
+    console.log(bst.insert(n));
+})
 bst.toString();

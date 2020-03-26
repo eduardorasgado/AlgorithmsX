bd965d28eff54c9bbf02fbfc19db3c9ebf57369f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-25 21:39:32 -0600

    queues: queue exam, unqueue method.

---
+++ DataStructures/queues/singlybased-queue.js
@@ -0,0 +1,54 @@
+const Utils = require("../logUtils");
+
+// implementing a queue using singly linked list
+// queues are LIFO structures
+
+// singly node
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    getValue() { return this.value; }
+    setValue(value) { this.value = value; }
+    getNext() { return this.next; }
+    setNext(next) { this.next = next; }
+    toString() { Utils.inspectObject(this) }
+}
+
+// queues are based on singly linked list operations
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    // inserting element into the queue
+    // using singly linked list push method
+    unqueue(value) {
+        if(value == undefined) return this;
+        let newNode = new Node(value);
+        if(!this.head) this.head = this.tail = newNode;
+        else {
+            this.tail.setNext(newNode);
+            this.tail = newNode;
+        }
+        ++this.length;
+        return this;
+    }
+
+    // removing the first element was inserted into the queue
+    dequeue() {
+
+    }
+
+    toString() { Utils.inspectObject(this) }
+}
+
+let q1 = new Queue();
+q1.unqueue("FIRST")
+q1.unqueue("SECOND")
+q1.unqueue("THIRD")
+q1.toString();

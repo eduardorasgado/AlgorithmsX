65d40cdaf334c72e8754a6cbd7fb2deaf302bca2
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-03-28 19:07:12 -0600

    binary heaps: min heap -> poll  and heapify down method.

--- DataStructures/trees/heaps/min-heap.js
+++ DataStructures/trees/heaps/min-heap.js
@@ -68,37 +68,64 @@ class MinHeap {
         this.items[firstIndex] -= this.items[secondIndex];
     }
 
-    // removing the minimum element
+    /**
+     * gettting the minimum element within the list
+     *
+     * @returns {null|*}
+     */
     peek() {
-        //
+        if(!this.size) return null;
+        return this.items[0];
     }
 
-    // removing the last element
+    // removing the minimum element
     poll() {
-        //
+        if(!this.size) return null;
+        let item = this.items[0];
+        this.items[0] = this.items[this.size - 1];
+        --this.size;
+        this.items.pop();
+        this.heapifyDown();
+        return item;
     }
 
-    // inserting a new element
+    /**
+     * inserting a new element
+     *
+     * @param value
+     */
     insert(value) {
         this.items[this.size] = value;
-        this.heapifyUp()
         ++this.size;
+        this.heapifyUp()
+        return this;
     }
 
     // bubbling from deeper level to root
     heapifyUp() {
-        let currentIndex = this.size
-        while(this.hasParent(currentIndex)) {
-            if(this.items[currentIndex] < this.items[this.getParentIndex(currentIndex)]) {
-                this.swap(currentIndex, this.getParentIndex(currentIndex));
-            }
+        let currentIndex = this.size - 1;
+        while(this.hasParent(currentIndex) &&
+        this.items[currentIndex] < this.items[this.getParentIndex(currentIndex)]) {
+            this.swap(currentIndex, this.getParentIndex(currentIndex));
             currentIndex = this.getParentIndex(currentIndex);
         }
     }
 
     // bubbling from the root element to the deeper level
     heapifyDown() {
-        //
+        let currentIndex = 0;
+        let smallerChildIndex;
+        while(this.hasLeftChild(currentIndex)) {
+            smallerChildIndex = this.getLeftChildIndex(currentIndex);
+            if(this.hasRightChild(currentIndex) &&
+                this.getRightChild(currentIndex) > this.getLeftChild(currentIndex)) {
+                smallerChildIndex = this.getRightChildIndex(currentIndex);
+            }
+            if(this.items[currentIndex] > this.items[smallerChildIndex])
+                this.swap(currentIndex, smallerChildIndex);
+            else break;
+            currentIndex = smallerChildIndex;
+        }
     }
 }
 
@@ -116,9 +143,14 @@ let mh = new MinHeap();
 // mh.swap(0, 1)
 // console.log(mh.items[0], mh.items[1]);
 
-let itemsToInsert = [13, 16, 31, 41, 51, 100, 41];
+let itemsToInsert = [13, 41, 100, 16, 51, 31, 41]
 
 itemsToInsert.map((element) => {
-    mh.insert(element);
+    console.log(mh.insert(element));
 })
+console.log("----");
+console.log(mh.peek());
+console.log(mh.poll());
+console.log("----");
 console.log(mh.items);
+console.log(mh.size);

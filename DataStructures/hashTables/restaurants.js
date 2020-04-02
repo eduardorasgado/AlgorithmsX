2f3a0cf7ce36bdbf51d7e6bab1af1b43aa63d9db
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-02 04:26:34 -0600

    Hash tables: adding a restaurant simple data sample to test out our ht.

---
+++ DataStructures/hashTables/restaurants.js
@@ -0,0 +1,142 @@
+let restaurants = [ {
+    "code": "mcdonald",
+    "item" : "MCDONALD'S",
+    "count" : "8"
+}, {
+    "code": "popeyes",
+    "item" : "POPEYES FAMOUS FRIED CHICKEN",
+    "count" : "7"
+}, {
+    "code": "subway",
+    "item" : "SUBWAY",
+    "count" : "6"
+}, {
+    "code": "kfc",
+    "item" : "KENTUCKY FRIED CHICKEN",
+    "count" : "5"
+}, {
+    "code": "rhh",
+    "item" : "RENAISSANCE HARBORPLACE HOTEL",
+    "count" : "4"
+}, {
+    "code": "bk",
+    "item" : "BURGER KING",
+    "count" : "4"
+}, {
+    "code": "ddonuts",
+    "item" : "DUNKIN DONUTS",
+    "count" : "4"
+}, {
+    "code": "donnas",
+    "item" : "DONNA'S",
+    "count" : "3"
+}, {
+    "code": "carrol",
+    "item" : "CARROLL STATION",
+    "count" : "2"
+}, {
+    "code": "fiveguys",
+    "item" : "FIVE GUYS FAMOUS BURGERS & FRIES",
+    "count" : "2"
+}, {
+    "code": "ruths",
+    "item" : "RUTH'S CHRIS STEAK HOUSE",
+    "count" : "2"
+}, {
+    "code": "austingll",
+    "item" : "AUSTIN GRILL",
+    "count" : "2"
+}, {
+    "code": "panera",
+    "item" : "PANERA BREAD",
+    "count" : "2"
+}, {
+    "code": "starbucks",
+    "item" : "STARBUCKS COFFEE",
+    "count" : "2"
+}, {
+    "code": "dgrind",
+    "item" : "DAILY GRIND",
+    "count" : "2"
+}, {
+    "code": "ruby",
+    "item" : "RUBY TUESDAY",
+    "count" : "2"
+}, {
+    "code": "wendys",
+    "item" : "WENDY'S",
+    "count" : "2"
+}, {
+    "code": "quizno",
+    "item" : "QUIZNO'S",
+    "count" : "2"
+}, {
+    "code": "down",
+    "item" : "Downtown",
+    "count" : "128"
+}, {
+    "code": "fells",
+    "item" : "Fells Point",
+    "count" : "91"
+}, {
+    "code": "inner",
+    "item" : "Inner Harbor",
+    "count" : "89"
+}, {
+    "code": "canton",
+    "item" : "Canton",
+    "count" : "81"
+}, {
+    "code": "federal",
+    "item" : "Federal Hill",
+    "count" : "42"
+}, {
+    "code": "mount",
+    "item" : "Mount Vernon",
+    "count" : "33"
+}, {
+    "code": "riverside",
+    "item" : "Riverside",
+    "count" : "29"
+}, {
+    "item" : "Mid-Town Belvedere",
+    "count" : "29"
+}, {
+    "item" : "Hampden",
+    "count" : "29"
+}, {
+    "item" : "Charles Village",
+    "count" : "25"
+}, {
+    "item" : "Little Italy",
+    "count" : "23"
+}, {
+    "item" : "Greektown",
+    "count" : "22"
+}, {
+    "item" : "Charles North",
+    "count" : "22"
+}, {
+    "item" : "Highlandtown",
+    "count" : "18"
+}, {
+    "item" : "Washington Village",
+    "count" : "17"
+}, {
+    "item" : "Brooklyn",
+    "count" : "17"
+}, {
+    "item" : "Carrollton Ridge",
+    "count" : "16"
+}, {
+    "item" : "Baltimore Highlands",
+    "count" : "16"
+}, {
+    "item" : "Baltimore-Linwood",
+    "count" : "15"
+}, {
+    "item" : "Brewers Hill",
+    "count" : "14"
+} ];
+
+exports.restaurants = restaurants;

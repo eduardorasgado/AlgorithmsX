6dd6fc3cab09d2387de42360e15518650bc31b45
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-02 23:55:33 -0600

    Graphs: Introduction to graphs.

---
+++ DataStructures/graphs/introduction-notes.js
@@ -0,0 +1,63 @@
+/**
+ *      GRAPHS
+ *
+ *          OBJECTIVES
+ *
+ *              Explain what a graph is
+ *              Compare and contrast different types of graphs and their use cases
+ *                  in the real world
+ *              Implement a graph using an adjancency list
+ *              Travers through a graph using BFS and DFS
+ *              Compare and contrast graph traversal algorithms
+ *
+ *          WHAT ARE GRAPHS
+ *
+ *              A graph is a non linear data structure cosisting of  nodes and edges.
+ *              The nodes are sometimes also referred to as vertices and the edges are
+ *              lines or arcs that connect any two nodes in the graph. More formally
+ *              a graph can be defined as:
+ *
+ *              A graph data structure consists of a finite(and possibly mutable)
+ *              set of edges which connect a pair of nodes or points, together with a set of unordered
+ *              pars of these vertices for an undirected graph or a set of ordered
+ *              pairs for a directed graph.
+ *
+ *              0_______1___
+ *              |       |   |__2
+ *              |       |  ___|
+ *              4_______3_|
+ *              In the above graph, the set of vertices V={0, 1, 2, 3, 4} and the
+ *              set of edges E= {01, 12, 23, 34, 04, 14, 13}
+ *
+ *              Graphs are used to represent networks.
+ *
+ *              Graphs are briefly: nodes + connections
+ *                   B___A____E
+ *                   |\__
+ *                   |  |
+ *              C____D  F
+ *
+ *              Graphs have no entry points
+ *
+ *          USES FOR GRAPHS
+ *
+ *              Social Networks
+ *              Location / Mapping
+ *              Routing Algorithms
+ *              Visual Hierarchy
+ *              File System Optimization
+ *              Path finding
+ *              Modeling internet
+ *              Recomendation engines
+ *              Everywhere...
+ *
+ *          ESSENTIAL GRAPH TERMS
+ *
+ *              Vertex - a node
+ *              Edge - connection between nodes
+ *              Weighted/ unweighted - values assigned to distances between vertices
+ *              Directed/ Undirected - directions assigned to distances between vertices
+ *
+ *          TYPES OF GRAPHS
+ *
+ * */

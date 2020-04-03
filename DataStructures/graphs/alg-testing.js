6c45ab0b16e973a5983273fb90f87c064ab31606
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-03 14:03:45 -0600

    graphs: addVertex and addEdge methods.

---
+++ DataStructures/graphs/alg-testing.js
@@ -0,0 +1,32 @@
+const { Graph } = require("./list-based-graph");
+
+let computerIds = [
+    '12.35.54.1',
+    '13.56.2.1',
+    '12.66.44.33',
+    '192.168.23.1',
+    '255.255.255.1',
+    '255.255.1.1',
+    '127.0.0.1'
+]
+
+let network = new Graph();
+
+// inserting vertexes
+computerIds.map((id) => {
+    network.addVertex(id);
+})
+
+// inserting edges to each vertexes
+computerIds.map((id) => {
+    let sampling = Math.floor(
+        (Math.random() * 100) % network.getSize())
+
+    for (let i = 0; i < sampling; i++) {
+        let edge = Math.floor(
+            (Math.random() * 100) % network.getSize());
+        // repetitions are avoided in addEdge method
+        network.addEdge(computerIds[edge], id)
+    }
+})
+console.log(network);
--- DataStructures/graphs/list-based-graph.js
+++ DataStructures/graphs/list-based-graph.js
@@ -1,3 +1,60 @@
 /**
  * We can represent a graph through a adjancency list.
+ *  Why?
+ *  Most data in the real world tends to lend itself to sparser and/or larger graphs
+ *
+ *  Undirected graph
+ *  @class
  * */
+class Graph {
+    constructor() {
+        // using built in JS hash table
+        this.adjancencyList = {};
+    }
+
+    /**
+     * Insert a new node
+     * STEPS:
+     *  Acceps a name of a vertex
+     *  It should add a key to the adjancency list with the name of the vertex
+     *  and set its value to be an empty array
+     */
+    addVertex(vertex) {
+        if(!this.adjancencyList[vertex])
+            this.adjancencyList[vertex] = [];
+    }
+
+    /**
+     * adding edges to between two vertexes, avoiding to repeat insertions
+     * @param vertex1
+     * @param vertex2
+     */
+    addEdge(vertex1, vertex2) {
+        // not inserting vertex itself
+        if(vertex1 !== vertex2) {
+            if(this.adjancencyList[vertex1] && this.adjancencyList[vertex2]) {
+                // verifying not repeating insertions
+                if(!this.adjancencyList[vertex1].includes(vertex2) &&
+                    !this.adjancencyList[vertex2].includes(vertex1)) {
+                    this.adjancencyList[vertex1].push(vertex2);
+                    this.adjancencyList[vertex2].push(vertex1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Get the amount of vertexes within the graph
+     * @returns {number}
+     */
+    getSize() {
+        let size = 0;
+        let vertex;
+        for(vertex in this.adjancencyList) {
+            ++size;
+        }
+        return size;
+    }
+}
+
+exports.Graph = Graph;

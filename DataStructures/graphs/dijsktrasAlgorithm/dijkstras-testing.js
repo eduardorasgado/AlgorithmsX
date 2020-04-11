07d8718cce33a79d7a0eb98c185622e57d5e569e
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-10 23:59:11 -0500

    Dijkstra's Algorithm: structuring THE TESTING

--- DataStructures/graphs/dijsktrasAlgorithm/dijkstras-testing.js
+++ DataStructures/graphs/dijsktrasAlgorithm/dijkstras-testing.js
@@ -5,4 +5,15 @@ let g = new Graph();
 ["A", "B", "C", "D", "E", "F"]
     .forEach(letter => g.addVertex(letter));
 
+let edges = [
+    ["A", "B", 4],
+    ["B", "E", 3],
+    ["E", "D", 3],
+    ["D", "C", 2],
+    ["A", "D", 2],
+    ["C", "A", 2],
+    ["C", "F", 4],
+    ["F", "D", 1],
+    ["F", "E", 1]
+];
 console.log(g);

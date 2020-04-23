1edf337c6860fa0f6a0f3db1ccdadf6e1bdac148
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-23 09:33:01 -0500

    Hacker rank: interview kit: arrays, array manipulation, second attempt prep.

--- hackerRank/interview-preparation-kit/arrays/array-manipulation.js
+++ hackerRank/interview-preparation-kit/arrays/array-manipulation.js
@@ -8,46 +8,77 @@ const { dataReader } = require('./array-manipulation-long-test-data/data-reader'
 function arrayManipulation(n, queries) {
     let arr = Array.from({length: n}, () => 0);
     let queriesLen = queries.length;
+    let maxAccumulate = 0;
     let maxValue = 0;
+    let maxValueRange = [0, 0]
     let startIdx, finalIdx, value;
+    let accumulativeForAll = 0;
+    let currentForAllValue = 0;
     for(let i = 0; i < queriesLen; i++) {
         startIdx = queries[i][0]-1;
         finalIdx = queries[i][1]-1;
         value = queries[i][2];
-        for(startIdx; startIdx<= finalIdx; startIdx++) {
-            arr[startIdx] += value;
-            maxValue = Math.max(maxValue, arr[startIdx]);
-        }
+        if(finalIdx === n-1 && startIdx === 0) {
+            console.log("Acc");
+            currentForAllValue = value
+            accumulativeForAll += currentForAllValue;
+            maxAccumulate += currentForAllValue;
+        } else {
+            if(value > maxValue) {
+                maxValue = value;
+                maxValueRange[0] = startIdx;
+                maxValueRange[1] = finalIdx;
+            }
+            if(value != 0) {
+                if(maxAccumulate < maxValue) {
+                    for(startIdx; startIdx <=  finalIdx; startIdx++) {
+                        arr[startIdx] += value;
+                        maxAccumulate = Math.max(maxAccumulate, arr[startIdx]+ accumulativeForAll);
+                    }
+                } else {
+                    // maxValue is now maxAccumulate
+                    startIdx = Math.max(startIdx, maxValueRange[0]);
+                    finalIdx = Math.min(finalIdx, maxValueRange[1]);
+                    for(startIdx; startIdx <=  finalIdx; startIdx++) {
+                        arr[startIdx] += value;
+                        maxAccumulate = Math.max(maxAccumulate, arr[startIdx]+ accumulativeForAll);
+                    }
+                }
 
+            }
+        }
     }
-    return maxValue;
+    console.log(maxValue, maxValueRange[0],maxValueRange[1]);
+    return maxAccumulate;
 }
 
 function longDataTestSuite() {
-    // 10000000 100000   the size of the array and the number of operations.
+    // 10 000 000, 100 000   the size of the array and the number of operations.
+    // r: 2497169732
     let queries1 = dataReader( "example1.txt");
     //console.log(queries1.slice(queries1.length-100, queries1.length));
+    console.log("response: ", arrayManipulation(10000000, queries1))  ;
 }
 
 function basicTestSuite() {
     // r: 200
     // 5 3
-    console.log(arrayManipulation(5,
+    console.log("basic suite here");
+    console.log("response: ", arrayManipulation(5,
         [[1, 2, 100],
             [2, 5, 100],
-            //[2, 2, 50],
             [3, 4, 100]]));
 
     // r: 10
     // 10 3
-    console.log(arrayManipulation(10,
+    console.log("response: ", arrayManipulation(10,
         [[1, 5, 3],
             [4, 8, 7],
             [6, 9, 1]]));
 
     // r: 31
     //10 4
-    console.log(arrayManipulation(10,
+    console.log("response: ", arrayManipulation(10,
         [[2, 6, 8],
             [3, 5, 7],
             [1, 8, 1],
@@ -64,9 +95,9 @@ longDataTestSuite();
 // 7 4 8
 // 1 6 9
 // 1  2  3  4  5  6  7  8  9  10
-//     [0  0  0  0  0  0  0  0  0  0]
+// [0  0  0  0  0  0  0  0  0  0]
 // [3  3  3  3  3  0  0  0  0  0]
-//     [3  3  3 10  10 7  7  7  0  0]
+// [3  3  3 10  10 7  7  7  0  0]
 // [3  3  3 10  10 8  8  8  1  0]
 
 // largest value = 10

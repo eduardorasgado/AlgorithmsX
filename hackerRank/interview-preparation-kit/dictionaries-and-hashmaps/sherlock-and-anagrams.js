fbca05e9a81de8ea9a0ea48c071281c266347da0
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-27 06:32:03 -0500

    Hacker rank: interview kit: dictionaries and hash maps, sherlock and anagrams, first version passed basic tests.

--- hackerRank/interview-preparation-kit/dictionaries-and-hashmaps/sherlock-and-anagrams.js
+++ hackerRank/interview-preparation-kit/dictionaries-and-hashmaps/sherlock-and-anagrams.js
@@ -12,60 +12,121 @@ function sherlockAndAnagrams(s) {
             alphaMap[letter] = (letter.codePointAt(0)-96)*37;
         }
     })(alpha);
-    console.log(alphaMap);
+    //console.log(alphaMap);
 
     let repetitionMap = {};
     let element;
     let i;
+
     for(i = 0; i< sLen; i++) {
         if(!repetitionMap[s[i]]) repetitionMap[s[i]] = [i];
-        else repetitionMap[s[i]].push(i);
+        else {
+            repetitionMap[s[i]].push(i);
+        }
     }
-    console.log(repetitionMap);
+    let boundaries = [];
+    let boundariesOwners = []
+    for(element in repetitionMap) {
+        if (repetitionMap.hasOwnProperty(element)) {
+            if (repetitionMap[element].length > 1) {
+                boundaries
+                    .push([repetitionMap[element][0],
+                        repetitionMap[element].slice(-1)[0]])
+                boundariesOwners.push(element)
+            }
+        }
+    }
 
-    let currentWindow, windowLen, j;
+    //console.log("before boundaries: ", repetitionMap);
+    //console.log(boundaries);
+    //console.log(boundariesOwners);
+
+    let j = 0;
     for(element in repetitionMap) {
         if(repetitionMap.hasOwnProperty(element)) {
-            if(repetitionMap[element].length > 1) {
-                console.log("--", repetitionMap[element]);
-                // to compare sums in window and determinate if there is a valid pair
-                let pairSum, lastPairSum;
-                i = repetitionMap[element][0];
-                let j = repetitionMap[element].slice(-1)[0];
-                let windowLen = 1;
-                // get through the window and increment window len until len = (j-i)-1
+            if(repetitionMap[element].length > 0) {
+                let bound;
+                j = 0;
+                for(bound of boundaries) {
+                    let upperBound = repetitionMap[element].slice(-1)[0];
+                    let lowerBound = repetitionMap[element][0];
+                    if((bound[0] > lowerBound && (upperBound < bound[1] && upperBound > bound[0]))
+                        || (((bound[0] < lowerBound) && (bound[1] > lowerBound))
+                            && bound[1] < upperBound)) {
+                        // merge the bounds
+                        if (bound[0] > lowerBound){
+                            // repetition map element will absorb bound boundariesOwners
+                            repetitionMap[element]
+                                .push(repetitionMap[boundariesOwners[j]].slice(-1)[0]);
 
-                // window length increments until reach
-                while(i+windowLen < j+1) {
-                    console.log("window len loop, ", s[i], i, windowLen);
-                    let k, q, z;
-                    let elements = [0, 0]
-
-                    for(k= i; k < j+1; k++) {
-                        elements[0] = 0;
-                        for(z = k; z < k + windowLen; z++){
-                            //console.log(z);
-                            if(s[z]) elements[0] += (s[z].codePointAt(0)-96) * 37;
+                            //delete repetitionMap[boundariesOwners[j]]
+                        } else {
+                            // boundaries owner will absorb repetition mape element
+                            repetitionMap[boundariesOwners[j]].push(repetitionMap[element].slice(-1)[0]);
+                            //delete repetitionMap[element];
                         }
-                        for(q= k+1; q < j+1; q++) {
-                            console.log(k, q);
-                            for(z = q; z < q + windowLen; z++){
-                                if(z < sLen) elements[1] += (s[z].codePointAt(0)-96) * 37;
-                                console.log("here: ",z);
-                            }
-                            //console.log("nested: ", s[k], k)
-                            if(elements[0] === elements[1]) {
-                                console.log("elements: ", elements);
-                                ++annPairs;
-                            }
-                            elements[1] = 0;
-                        }
+                        //console.log("bounds crossed----------")
                     }
-                    ++windowLen;
+                    ++j;
                 }
             }
         }
     }
+    //console.log(repetitionMap);
+
+    let reviewed = {}
+    let currentWindow, windowLen;
+    for(element in repetitionMap) {
+        if(repetitionMap.hasOwnProperty(element)) {
+            if(repetitionMap[element].length > 1) {
+                //console.log("--", repetitionMap[element]);
+                // to compare sums in window and determinate if there is a valid pair
+                let pairSum, lastPairSum;
+                i = repetitionMap[element][0];
+                j = repetitionMap[element].slice(-1)[0];
+                if(!(reviewed[i] && reviewed[j])) {
+                    let windowLen = 1;
+                    // get through the window and increment window len until len = (j-i)-1
+
+                    // window length increments until reach
+                    while(i+windowLen < j+1) {
+                        //console.log("window len loop, ", s[i], i, windowLen);
+                        let k, q, z;
+                        let elements = [0, 0]
+
+                        for(k= i; k < j+1; k++) {
+                            elements[0] = 0;
+                            for(z = k; z < k + windowLen; z++){
+                                //console.log(z);
+                                if(s[z]) {
+                                    elements[0] += (s[z].codePointAt(0)-96) * 37;
+                                    reviewed[k] = true;
+                                }
+                            }
+                            for(q= k+1; q < j+1; q++) {
+                                //console.log(k, q);
+                                for(z = q; z < q + windowLen; z++){
+                                    if(z < sLen) {
+                                        elements[1] += (s[z].codePointAt(0)-96) * 37;
+                                        reviewed[q] = true;
+                                    }
+                                    //console.log("here: ",z);
+                                }
+                                //console.log("nested: ", s[k], k)
+                                if(elements[0] === elements[1]) {
+                                    //console.log("elements: ", elements);
+                                    ++annPairs;
+                                }
+                                elements[1] = 0;
+                            }
+                        }
+                        ++windowLen;
+                    }
+                } // else console.log("element reviewed");
+            }
+        }
+    }
+    //console.log(reviewed);
 
     return annPairs;
 
@@ -75,13 +136,13 @@ function basicTestSuite() {
     //
     // 4
     // 0
-    //console.log(sherlockAndAnagrams('abba'));
-    //console.log(sherlockAndAnagrams('abcd'));
+    console.log(sherlockAndAnagrams('abba'));
+    console.log(sherlockAndAnagrams('abcd'));
 
     // 3
     // 10
-    //console.log(sherlockAndAnagrams('ifailuhkqq'));
-    //console.log(sherlockAndAnagrams('kkkk'));
+    console.log(sherlockAndAnagrams('ifailuhkqq'));
+    console.log(sherlockAndAnagrams('kkkk'));
 
     // 5
     console.log(sherlockAndAnagrams('cdcd'));

e6a9587c4f4dd05177b8f9204ad8945e172c461f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-05-03 23:57:25 -0500

    Hacker rank: interview kit: dictionaries and hash maps, count triplets, paused. Cats and mouse test suite. All test passed.

--- hackerRank/solving-problem/easy/cats-and-mouse.js
+++ hackerRank/solving-problem/easy/cats-and-mouse.js
@@ -1,15 +1,22 @@
 // https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
 // Complete the catAndMouse function below.
 function catAndMouse(x, y, z) {
-    //
+    let catAProb = [Math.abs(z- x), "Cat A"];
+    let catBProb = [Math.abs(z - y), 'Cat B'] ;
+    if(catAProb[0] == catBProb[0]) return "Mouse C";
+    else if (Math.min(catAProb[0], catBProb[0]) == catAProb) {
+        return catAProb[1];
+    } else {
+        return catBProb[1];
+    }
 }
 
 function basicTestSuite() {
     // Cat B
-    catAndMouse(1, 2, 3)
+    console.log(catAndMouse(1, 2, 3));
     // Mouse C
     //1 3 2
-    catAndMouse(1, 3, 2)
+    console.log(catAndMouse(1, 3, 2));
 }
 
 basicTestSuite();

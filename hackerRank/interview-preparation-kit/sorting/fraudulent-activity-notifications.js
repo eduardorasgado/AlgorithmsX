70753e925986241577637dbc3e866bb07fb0febf
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-05-08 03:39:03 -0500

    Hacker rank: interview kit: sorting, fraudulent activity notifications, semi optimized version, 40 secs for 200k elements, still slow.

--- hackerRank/interview-preparation-kit/sorting/fraudulent-activity-notifications.js
+++ hackerRank/interview-preparation-kit/sorting/fraudulent-activity-notifications.js
@@ -5,53 +5,24 @@ function activityNotifications(expenditure, d) {
     let expLen = expenditure.length;
     let notificationCounter = 0;
     // first, order the elements
-    // using radix sort
+    // using insertion sort due to first all but first time, window is
+    // partially sorted
     function sortWindow(arr, toDeleteExp) {
-        // calculate a msd
         let arrLen = arr.length;
+        let currentVal;
         let toDeleteIdx = 0;
-        let i;
-        let msd = 0;
-        // getting the most significat digit from all the numbers in array
-        for(i = 0; i < arrLen; i++) {
-            msd = Math.max(msd,
-                Math.max(Math.floor(Math.log10(Math.abs(arr[i]))) , 0) + 1
-            );
-        }
-        // sorting loop based
-        // sorting using buckets
-        let currentDigit;
-        let j;
-        let currentPos = 0;
-        let currentBucket
-        let bucketsLength; // store the length of all the buckets
-        let bucketPresent = 0; // stores the bucket in where toDeleteExp was found
-        for(i = 0; i < msd; i++){
-            let buckets = Array.from({length: 10}, () => []);
-            bucketsLength = Array.from({length: 10}, () => 0);
-            for (j = 0; j < arrLen; j++) {
-                // sorting using the current digit at position i for every element in arr
-                currentDigit = Math.floor(
-                    (Math.abs(arr[j] / Math.pow(10, i))) % 10
-                );
-                buckets[currentDigit].push(arr[j]);
-                ++bucketsLength[currentDigit];
-                
-                if(arr[j] === toDeleteExp) {
-                    bucketPresent = currentDigit;
-                    toDeleteIdx = bucketsLength[currentDigit] - 1;
-                }
+        for(let i = 1; i < arrLen; i++) {
+            currentVal = arr[i];
+            let j;
+            let changed = false;
+            for (j = i - 1; j >= 0 && arr[j] > currentVal ; j--) {
+                arr[j+1] = arr[j];
+                if(arr[j+1] === toDeleteExp) toDeleteIdx = j+1;
             }
-
-            arr = [].concat(...buckets);
+            arr[j+1] = currentVal;
+            if(currentVal === toDeleteExp) toDeleteIdx = j+1;
         }
-        j = 0;
-        for(let j; j < 10; j++) {
-            if(bucketsLength[j] < bucketPresent) {
-                 toDeleteIdx += bucketsLength[j];
-            }
-        }
-        //console.log(arr, toDeleteIdx)
+        //console.log(arr, toDeleteIdx, toDeleteExp);
         return [arr, toDeleteIdx];
     }
     // get the window of d elements over the expeditures
@@ -71,7 +42,8 @@ function activityNotifications(expenditure, d) {
     while (i <= expLen - d - 1){
         // sorting the window
         // sortWindow return location of lastLocator[i] to be able to delete it at O(1)
-        [sortedWindow, iDelete] = sortWindow(sortedWindow, lastLocator[i-1]);
+        [sortedWindow, iDelete] = sortWindow(sortedWindow, lastLocator[i]);
+        //console.log(sortedWindow);
         //console.log(lastLocator[i], iDelete);
         //every time calculate the median for elements within window
         if(d % 2 === 0) {

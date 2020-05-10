b18eea6404d6f529356acc1ec3adc3c4bdcf3356
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-05-09 20:13:32 -0500

    Hacker rank: interview kit: sorting, fraudulent activity notifications, another version.

--- hackerRank/interview-preparation-kit/sorting/fraudulent-activity-notifications.js
+++ hackerRank/interview-preparation-kit/sorting/fraudulent-activity-notifications.js
@@ -4,33 +4,33 @@ let example1 = require("./fraudulent-activity-long-data-test/example1");
 function activityNotifications(expenditure, d) {
     let expLen = expenditure.length;
     let notificationCounter = 0;
+    let windowPosLocator = {};
     // first, order the elements
     // using insertion sort due to first all but first time, window is
     // partially sorted
-    function sortWindow(arr, toDeleteExp) {
+    function sortWindow(arr) {
         let arrLen = arr.length;
         let currentVal;
-        let toDeleteIdx = 0;
         for(let i = 1; i < arrLen; i++) {
             currentVal = arr[i];
             let j;
             let changed = false;
             for (j = i - 1; j >= 0 && arr[j] > currentVal ; j--) {
                 arr[j+1] = arr[j];
-                if(arr[j+1] === toDeleteExp) toDeleteIdx = j+1;
             }
             arr[j+1] = currentVal;
-            if(currentVal === toDeleteExp) toDeleteIdx = j+1;
         }
         //console.log(arr, toDeleteIdx, toDeleteExp);
-        return [arr, toDeleteIdx];
+        return arr;
     }
     // get the window of d elements over the expeditures
     let i;
     let j = d;
     let lastLocator = []; // to locate the last shifted
     let sortedWindow = [];
+    let max = 0;
     for(i = 0; i < j; i++) {
+        (i === 0) ? max = expenditure[i] : Math.max(max, expenditure[i]);
         sortedWindow[i] = expenditure[i];
         lastLocator[i] = expenditure[i];
     }
@@ -39,10 +39,16 @@ function activityNotifications(expenditure, d) {
     let median, median2;
     i = 0;
     let iDelete = 0;
+
     while (i <= expLen - d - 1){
         // sorting the window
         // sortWindow return location of lastLocator[i] to be able to delete it at O(1)
-        [sortedWindow, iDelete] = sortWindow(sortedWindow, lastLocator[i]);
+        if(i === 0 || sortedWindow[sortedWindow.length-1] < max){
+            //console.log(sortedWindow[sortedWindow.length - 1], max);
+            sortedWindow = sortWindow(sortedWindow);
+            max = sortedWindow[sortedWindow.length-1];
+            //console.log(sortedWindow);
+        }
         //console.log(sortedWindow);
         //console.log(lastLocator[i], iDelete);
         //every time calculate the median for elements within window
@@ -60,10 +66,11 @@ function activityNotifications(expenditure, d) {
         // sliding the window
         lastLocator.push(expenditure[j]);
         //remove from sortedWindow
-        [sortedWindow[iDelete], sortedWindow[d-1]] =
-            [sortedWindow[d-1], sortedWindow[iDelete]];
-        sortedWindow.pop();
-        sortedWindow.push(expenditure[j])
+        //[sortedWindow[iDelete], sortedWindow[d-1]] =
+        //    [sortedWindow[d-1], sortedWindow[iDelete]];
+        //sortedWindow.pop();
+        sortedWindow.splice(sortedWindow.indexOf(lastLocator[i]), 1);
+        sortedWindow.push(expenditure[j]);
         ++i;
         ++j; // j is the right number next to window
     }
